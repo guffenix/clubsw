ac68fc57ab5341abf6ef91569e636f8ce25a7514
guffenix <guffenix@gmail.com>
2017-03-27 21:57:38 -0500

    versión 1.0 página eventos externa

---
+++ CLUBSW/index.php
@@ -0,0 +1,172 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Club de Software - ESPE</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Sidebar -->
+			<section id="sidebar">
+				<div class="inner">
+					<nav>
+						<ul>
+							<li><a href="#intro">Inicio</a></li>
+							<li><a href="#one">Áreas de Conocimiento</a></li>
+							<li><a href="#two">Próximos Eventos</a></li>
+							<li><a href="#three">Contáctanos</a></li>
+						</ul>
+					</nav>
+				</div>
+			</section>
+
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Intro -->
+					<section id="intro" class="wrapper style1 fullscreen fade-up">
+						<div class="inner">
+							<h1>Club de Software</h1>
+							<p>Este es el momento de formar parte del Club de Software <br>
+Te invita a participar en el Campamento Vacacional "System Camp 1.0"</p>
+<ul>
+<li> Aplicaciones Móviles</li>
+<li> Modelamiento en 3D y Realidad Aumentada</li>
+<li> Hacking</li>
+<li> Robótica</li>
+<li> Actividades Recreativas</li>
+</ul>
+
+							<ul class="actions">
+								<li><a href="https://goo.gl/forms/jvYcFWa3C3iSqZO72" target="_blank" class="button scrolly">Llenar Inscripción</a></li>
+							</ul>
+						</div>
+					</section>
+
+				<!-- One -->
+					<section id="one" class="wrapper style2 spotlights">
+						<section>
+							<a href="#" class="image"><img src="images/pic01.jpg" alt="" data-position="center center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Célula Microsoft</h2>
+									<p>Enfocados en el desarrollo de proyectos mediante herramientas Microsoft.</p>
+									
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic02.jpg" alt="" data-position="top center" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Célula Oracle</h2>
+									<p>Enfocados en el desarrollo de proyectos mediante herramientas Oracle.</p>
+									
+								</div>
+							</div>
+						</section>
+						<section>
+							<a href="#" class="image"><img src="images/pic03.jpg" alt="" data-position="25% 25%" /></a>
+							<div class="content">
+								<div class="inner">
+									<h2>Célula IOT</h2>
+									<p>Grupo especializado en la elaboración y desarrollo de proyectos del mundo interconectado. "Internet Of Things"</p>
+									
+								</div>
+							</div>
+						</section>
+					</section>
+
+				<!-- Two -->
+					<section id="two" class="wrapper style3 fade-up">
+						<div class="inner">
+							<h2>Próximos Eventos</h2>
+							<p>El Club de Software de la Universidad de las Fuerzas Armadas ESPE estará presente en los siguiente eventos, no te los puedes perder!</p>
+							<div class="features">
+								<section>
+									<span class="icon major fa-camera-retro "></span>
+									<h3>System Camp 1.0</h3>
+									<p>Campamento Vacacional el cual presenta a todo el público los beneficios tecnológicos de los últimos tiempos.</p>
+								</section>
+								<section>
+									<span class="icon major fa-diamond"></span>
+									<h3>Azure Day</h3>
+									<p>Evento de aprendizaje de Microsoft Azure.<br>
+Es la nube para los negocios modernos. Abierto, flexible, escalable, está lleno de infinitas posibilidades. 
+</p>
+								</section>
+								
+								
+							</div>
+							
+						</div>
+					</section>
+
+				<!-- Three -->
+					<section id="three" class="wrapper style1 fade-up">
+						<div class="inner">
+							<h2>Contáctanos</h2>
+							<p>No lo dudes y anímate a formar parte del Club de Software. Ahora!</p>
+							<div class="split style1">
+								<section>
+									
+
+									<ul class="contact">
+										<li>
+											<h3>Dirección</h3>
+											<span>Campus Matriz (Sangolquí)</span>
+										</li>
+										<li>
+											<h3>Correo</h3>
+											<a href="#">celulaprogrmacion@gmail.com</a>
+										</li>
+										<li>
+											<h3>Teléfonos</h3>
+											<span>(+593) 990655252 <br>(+593) 984872069</span>
+										</li>
+										<li>
+								</section>
+								<section>											<h3>Nuestras Redes Sociales</h3>
+											<ul class="icons">
+												<!--li><a href="#" class="fa-twitter"><span class="label">Twitter</span></a></li-->
+												<li><a href="#" class="fa-facebook"> /CMUESPE <span class="label">Facebook</span></a></li>
+<li><a href="#" class="fa-facebook"> /ORACLEESPE <span class="label">Facebook</span></a></li>
+
+
+												<!-- li><a href="#" class="fa-github"><span class="label">GitHub</span></a></li -->
+												<!-- li><a href="#" class="fa-instagram"><span class="label">Instagram</span></a></li -->
+												<!-- li><a href="#" class="fa-linkedin"><span class="label">LinkedIn</span></a></li -->
+																				</li>
+									</ul>
+								</section>
+							</div>
+						</div>
+					</section>
+
+			</div>
+
+		<!-- Footer -->
+			<footer id="footer" class="wrapper style1-alt">
+				<div class="inner">
+					<ul class="menu">
+						<li>&copy; CMUE. All rights reserved.</li><li> Sangolquí - Ecuador</li>
+					</ul>
+				</div>
+			</footer>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
